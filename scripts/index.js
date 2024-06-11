465e3a7bdc9ae09814dbb98ee3943287da3a71a2
unknown <cayetanogabriel03@gmail.com>
2024-06-11 21:28:18 +0800

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".side-bar").style.right = 0;
+})
+
+window.addEventListener("click", event => {
+  if(event.target.classList[2] != "hamburger-menu"){
+    document.querySelector(".side-bar").style.right = "-100%";
+  }
+
+})
