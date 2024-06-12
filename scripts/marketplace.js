215e42c37885a016d96b064cb07f3f55fb86e2e7
unknown <cayetanogabriel03@gmail.com>
2024-06-12 10:47:52 +0800

    added javascript for scroll animation

---
+++ scripts/marketplace.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', () => {
+    let container = document.querySelectorAll('.fade-in');
+  
+    for (let i of container){
+        let wHeight = window.innerHeight;
+        let revealTop = i.getBoundingClientRect().top;
+        let revealPoint = 250;
+  
+        if (revealTop < wHeight - revealPoint){
+            i.classList.add('reveals')
+        }
+    }
+  })
