d391b0b1270ebac7a5e12a525d5e641737a422f1
unknown <cayetanogabriel03@gmail.com>
2024-06-12 13:36:55 +0800

    removed pointer events when sidebar is active

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".side-bar").style.right = 0;
   document.querySelector("main").style.filter = "blur(5px)";
+  document.querySelector("main").style.pointerEvents = "none";
 })
 
 window.addEventListener("click", event => {
   if(event.target.classList[2] != "hamburger-menu"){
     document.querySelector(".side-bar").style.right = "-100%";
     document.querySelector("main").style.filter = "none";
-
+    document.querySelector("main").style.pointerEvents = "auto";
   }
 
 })
