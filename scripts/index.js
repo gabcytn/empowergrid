c41b502ee89b1dcffb7e9ec296e849e008239754
unknown <cayetanogabriel03@gmail.com>
2024-06-12 13:03:34 +0800

    added education section

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".side-bar").style.right = 0;
+  document.querySelector("main").style.filter = "blur(5px)";
 })
 
 window.addEventListener("click", event => {
   if(event.target.classList[2] != "hamburger-menu"){
     document.querySelector(".side-bar").style.right = "-100%";
+    document.querySelector("main").style.filter = "none";
+
   }
 
 })
